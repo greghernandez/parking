d3fe059e9f318847113f8aa44e36f171ace52898
luismontesd <luismontes655@gmail.com>
2017-11-29 13:48:07 -0600

    Update mapa.js

--- js/mapa.js
+++ js/mapa.js
@@ -1,12 +1,35 @@
 /**--------------------mapa----------------------**/
-    alert(position.coords.latitude);
+
+function geolocalizar(){            
+    //COMPROBAR QUE SE PUEDE GEOLOCALIZAR
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(posicionUsuario);
+    }
+}
+        
+function posicionUsuario(coordenadas){
+    latUser= coordenadas.coords.latitude;
+    longUser= coordenadas.coords.longitude; 
+    localStorage.setItem('latUser',latUser);
+    localStorage.setItem('longUser',longUser);
+}
+
+
       function initAutocomplete() {
+        lat = localStorage.getItem('latUser');
+        long = localStorage.getItem('longUser');
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 20.5911159, lng: -100.4023474},
-          zoom: 15,
+          center: {lat: parseFloat(lat), lng: parseFloat(long)},
+          zoom: 16,
           mapTypeId: 'roadmap',
           disableDefaultUI: true
-        });
+        });                           
+        var marker = new google.maps.Marker({
+            position:{lat: parseFloat(lat), lng: parseFloat(long)},
+            icon: "assets/icons/locationp.png",
+            map: map
+        });                          
+        
         // Create the search box and link it to the UI element.
         var input = document.getElementById('pac-input');
         var searchBox = new google.maps.places.SearchBox(input);
@@ -26,11 +49,6 @@
           if (places.length == 0) {
             return;
           }
-
-          // Clear out the old markers.
-          markers.forEach(function(marker) {
-            marker.setMap(null);
-          });
           markers = [];
 
           // For each place, get the icon, name and location.
