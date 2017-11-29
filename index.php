7e4ce477ef37bd0886849bb10515bd7344113351
greghernandez <gregoryarthur13@gmail.com>
2017-11-28 20:18:56 -0600

    Container Mapa
    COntainer para que el mapa se vea de tamaño completo

--- index.php
+++ index.php
@@ -8,13 +8,13 @@ require 'header.php';
 	</div>
 </div>
 </div>
-    <div id="contenedor">
-	<input id="pac-input" class="controls" type="text" placeholder="Busca un lugar...">
-    	<div id="map"></div>
-    	<script>window.addEventListener('load',geolocalizar,true);</script>
-    	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtsRprziIxG4nRzFBixVhtkYVcJrb40Bo&libraries=places&callback=initAutocomplete"
+    <div class="cont-map">
+	     <input id="pac-input" class="controls" type="text" placeholder="Busca un lugar...">
+    	 <div id="map"></div>
+    	 <script>window.addEventListener('load',geolocalizar,true);</script>
+    	 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtsRprziIxG4nRzFBixVhtkYVcJrb40Bo&libraries=places&callback=initAutocomplete"
          async defer></script>
-      	</div>
+    </div>
     </div>
 <?php
 require 'menu.php';
