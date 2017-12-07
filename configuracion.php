07d8190b12cb720d79b94ddf58b0e5c3592b8b41
greghernandez <gregoryarthur13@gmail.com>
2017-12-07 08:58:32 -0600

    config

--- configuracion.php
+++ configuracion.php
@@ -1,6 +1,28 @@
 <?php
 require 'header.php';
+
+session_start();
+$id_usuario = $_SESSION['user']; /*Remplazar con un session ID*/
+include ('conexion.php');
+    $con = Conectarse();
+    $qry= "SELECT * FROM usuario WHERE num_usuario = $id_usuario;";
+    $result = $con->query($qry);
+    while ($row = mysqli_fetch_array($result)) {
+        $nombre_usuario = $row['nombre_usuario'];
+        $apellido = $row['apellido_usuario'];
+        $correo = $row['correo'];
+        $telefono = $row['telefono'];
+        $no_tarjeta = $row['no_tarjeta'];
+    }
+    $qry= "SELECT * FROM auto WHERE id_usuario = $id_usuario;";
+    $result = $con->query($qry);
+    while ($row = mysqli_fetch_array($result)) {
+        $placa = $row['placa'];
+        $modelo = $row['modelo'];
+    }
+
 ?>
+
 	<div class="nav-title">
 		<span class="title big">Configuración</span>	
 	</div>
@@ -8,9 +30,22 @@ require 'header.php';
 </div>
     <div id="contenedor">
     	<div class="card animated bounceInDown">
-    		<p class="card-title">Edita la información de tu cuenta</p>
-    		<input type="text">
+            <p class="card-title">Información personal:</p>
+            <p><span class="bold">Nombre:  </span><input type="text" value="<?php echo $nombre_usuario; ?>" placeholder="Escribe tu nombre"></p> 
+            <p><span class="bold">Apellidos:  </span><input type="text" value="<?php echo $apellido; ?>" placeholder="Escribe tu epellido"></p>
+            <hr>
+            <p><span class="bold">Email:  </span><input type="email" value="<?php echo $correo; ?>" placeholder="Escribe tu email"></p>
+            <p><span class="bold">Teléfono:  </span><input type="text" value="<?php echo $telefono; ?>" placeholder="Escribe tu Teléfono"></p>
     	</div>
+        <div class="card animated bounceInDown">
+            <p class="card-title">Coche:</p>
+            <p><span class="bold">Modelo:  </span><?php echo $modelo ?></p>
+            <p><span class="bold">Placa:  </span><?php echo $placa ?></p>
+        </div>
+        <div class="card animated bounceInDown">
+            <p class="card-title">Contraseña:</p>
+            <p></p>
+        </div>
     </div>
 <?php
 require 'menu.php';
