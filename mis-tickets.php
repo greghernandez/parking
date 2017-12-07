86c49185b156c831e77b0f4b527c3bcced04c557
luismontesd <luismontes655@gmail.com>
2017-12-06 20:06:04 -0600

    ...

--- mis-tickets.php
+++ mis-tickets.php
@@ -1,5 +1,79 @@
     <?php
     require 'header.php';
+    $tiempo=$_GET['Tiempo'];
+    switch ($tiempo) {
+        case 0.15:
+            $minuto=15;
+            $hora=0;
+            break;
+        case 0.30:
+            $minuto=30;
+            $hora=0;
+            break;
+        case 0.45:
+            $minuto=45;
+            $hora=0;
+            break;
+        case 1:
+            $minuto=0;
+            $hora=1;
+            break;
+        case 1.15:
+            $minuto=15;
+            $hora=1;
+            break;
+        case 1.30:
+            $minuto=30;
+            $hora=1;
+            break;
+        case 1.45:
+            $minuto=45;
+            $hora=1;
+            break;
+        case 2:
+            $minuto=0;
+            $hora=2;
+            break;
+        case 2.15:
+            $minuto=15;
+            $hora=2;
+            break;
+        case 2.30:
+            $minuto=30;
+            $hora=2;
+            break;
+        case 2.45:
+            $minuto=45;
+            $hora=2;
+            break;
+        case 3:
+            $minuto=0;
+            $hora=3;
+            break;
+        case 3.15:
+            $minuto=15;
+            $hora=3;
+            break;
+        case 3.30:
+            $minuto=30;
+            $hora=3;
+            break;
+        case 3.45:
+            $minuto=45;
+            $hora=3;
+            break;
+        case 4:
+            $minuto=0;
+            $hora=4;
+            break;
+            
+    }
+if($minuto<10){
+    $minuto="0".$minuto;
+}
+if($hora<10){
+    $hora="0".$hora;
+}
     ?>
         <div class="nav-title">
             <span class="title">Mis tickets</span>  
@@ -31,9 +105,9 @@
                     <div class="col-4 center">
                         <p class="bold">Tiempo Restante: <br> <span class="time">
                             <div class="cont-reloj">
-                            <div class="reloj" id="Horas">00</div>
-		                    <div class="reloj" id="Minutos">:00</div>
-		                    <div class="reloj" id="Segundos">:00</div>
+                            <div class="reloj" id="Horas"><?php echo $hora ?></$php></div>
+		                    <div class="reloj" id="Minutos">:<?php echo $minuto ?></div>
+		                    <div class="reloj" id="Segundos">:60</div>
                             </div>
                             
                             </span></p>
@@ -46,6 +120,66 @@
             </div>
             
         </div>
+<script>
+window.addEventListener('load', inicio, true)
+var centesimas = 0;
+var segundos = 60;
+var minutos = <?php echo $minuto?>;
+var horas = <?php echo $hora?>;
+function inicio () {
+	control = setInterval(cronometro,10);
+	document.getElementById("inicio").disabled = true;
+	document.getElementById("parar").disabled = false;
+	document.getElementById("continuar").disabled = true;
+	document.getElementById("reinicio").disabled = false;
+}
+function parar () {
+	clearInterval(control);
+	document.getElementById("parar").disabled = true;
+	document.getElementById("continuar").disabled = false;
+}
+
+function cronometro () {
+	if ((segundos ==0)&&(minutos==0)&&(horas==0)){
+		parar();
+	}
+	if ( (centesimas == 0)&&(segundos == 0)&&(minutos == 0) ) {
+		horas=horas-1;
+		minutos= 61;
+		if (horas < 10) { horas = "0"+horas }
+		Horas.innerHTML = horas;
+	}
+	if (minutos == (minutos-1)) {
+		minutos = -1;
+	}
+	if ( (centesimas == 0)&&(segundos == 0) ) {
+		minutos = minutos-1;
+		segundos=61;
+		
+		if (minutos < 10) { minutos = "0"+minutos }
+		Minutos.innerHTML = ":"+minutos;
+	}
+	if (segundos == (segundos-1)) {
+		segundos = -1;
+	}
+	if (centesimas == 0) {
+		segundos=segundos-1;
+		if (segundos < 10) { segundos = "0"+segundos }
+		Segundos.innerHTML = ":"+segundos;
+	}
+	if (centesimas == 99) {
+		centesimas = -1;
+	}
+	if (centesimas < 99) {
+		centesimas++;
+		if (centesimas < 10) { centesimas = "0"+centesimas }
+		Centesimas.innerHTML = ":"+centesimas;
+	}
+
+
+
+}
+</script>
     <?php
     require 'menu.php';
     ?>
---
+++ pruebas/reloj.php
@@ -0,0 +1,134 @@
+
+<?php 
+	echo $hora=1;
+	echo $minuto=2;
+if($minuto<10){
+$minuto="0".$minuto;
+}
+if($hora<10){
+$hora="0".$hora;
+}
+?>
+<div id="contenedor">
+		<div class="reloj" id="Horas"><?php echo $hora;?></div>
+		<div class="reloj" id="Minutos">:<?php echo $minuto;?></div>
+		<div class="reloj" id="Segundos">:60</div>
+		<div class="reloj" id="Centesimas">:100</div>
+		<input type="button" class="boton" id="inicio" value="Start &#9658;" onclick="inicio();">
+		<input type="button" class="boton" id="parar" value="Stop &#8718;" onclick="parar();" disabled>
+		<input type="button" class="boton" id="continuar" value="Resume &#8634;" onclick="inicio();" disabled>
+		<input type="button" class="boton" id="reinicio" value="Reset &#8635;" onclick="reinicio();" disabled>
+	</div>
+
+	<style>
+*{
+	margin: 0;
+	padding: 0;
+}
+#contenedor{
+	margin: 10px auto;
+	width: 540px;
+	height: 115px;
+}
+.reloj{
+	float: left;
+	font-size: 80px;
+	font-family: Courier,sans-serif;
+	color: #363431;
+}
+.boton{
+	outline: none;
+	border: 1px solid #363431;
+	color: white;
+	width: 128px;
+	height: 30px;
+	text-shadow: 0px -1px 1px black;
+	font-size: 20px;
+	border-radius: 5px;
+	font-family: Helvetica;
+	cursor: pointer;
+	background-image: linear-gradient(#3aad02,#2c6f05);
+}
+.boton:active{
+	background-image: linear-gradient(#2c6f05,#3aad02);
+}
+.boton:hover{
+	box-shadow: 0px 0px 14px #3aad02;
+}
+
+
+	</style>
+<script>
+
+var centesimas = 0;
+var segundos = 5;
+var minutos = <?php echo $minuto?>;
+var horas = <?php echo $hora?>;
+function inicio () {
+	control = setInterval(cronometro,10);
+	document.getElementById("inicio").disabled = true;
+	document.getElementById("parar").disabled = false;
+	document.getElementById("continuar").disabled = true;
+	document.getElementById("reinicio").disabled = false;
+}
+function parar () {
+	clearInterval(control);
+	document.getElementById("parar").disabled = true;
+	document.getElementById("continuar").disabled = false;
+}
+function reinicio () {
+	clearInterval(control);
+	centesimas = 0;
+	segundos = 0;
+	minutos = 0;
+	horas = 0;
+	Centesimas.innerHTML = ":00";
+	Segundos.innerHTML = ":60";
+	Minutos.innerHTML = ":"+<?php echo $minuto?>;
+	Horas.innerHTML = <?php echo $minuto?>;
+	document.getElementById("inicio").disabled = false;
+	document.getElementById("parar").disabled = true;
+	document.getElementById("continuar").disabled = true;
+	document.getElementById("reinicio").disabled = true;
+}
+function cronometro () {
+	if ((segundos ==0)&&(minutos==0)&&(horas==0)){
+		parar();
+	}
+	if ( (centesimas == 0)&&(segundos == 0)&&(minutos == 0) ) {
+		horas=horas-1;
+		minutos= 2;
+		if (horas < 10) { horas = "0"+horas }
+		Horas.innerHTML = horas;
+	}
+	if (minutos == (minutos-1)) {
+		minutos = -1;
+	}
+	if ( (centesimas == 0)&&(segundos == 0) ) {
+		minutos = minutos-1;
+		segundos=5;
+		
+		if (minutos < 10) { minutos = "0"+minutos }
+		Minutos.innerHTML = ":"+minutos;
+	}
+	if (segundos == (segundos-1)) {
+		segundos = -1;
+	}
+	if (centesimas == 0) {
+		segundos=segundos-1;
+		if (segundos < 10) { segundos = "0"+segundos }
+		Segundos.innerHTML = ":"+segundos;
+	}
+	if (centesimas == 99) {
+		centesimas = -1;
+	}
+	if (centesimas < 99) {
+		centesimas++;
+		if (centesimas < 10) { centesimas = "0"+centesimas }
+		Centesimas.innerHTML = ":"+centesimas;
+	}
+
+
+
+}
+</script>
